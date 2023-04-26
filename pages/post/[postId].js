6a00031078a6a51ec15e83c2c64ffa2cce8322fa
casecam <109696610+casecam@users.noreply.github.com>
2023-04-26 15:36:46 -0700

    protected routes

---
+++ pages/post/[postId].js
@@ -0,0 +1,13 @@
+import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+
+export default function Post() {
+  return <div>
+    <h1>Post Page</h1>
+  </div>;
+}
+
+export const getServerSideProps = withPageAuthRequired(() => {
+  return {
+    props: {},
+  };
+});
---
+++ pages/token-topup.js
@@ -0,0 +1,13 @@
+import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+
+export default function TokenTopup() {
+  return <div>
+    <h1>Token Topup Page</h1>
+  </div>;
+}
+
+export const getServerSideProps = withPageAuthRequired(() => {
+  return {
+    props: {},
+  };
+});
