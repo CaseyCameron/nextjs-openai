35c6f88e0306cf75180374ed0370d3335034d769
casecam <109696610+casecam@users.noreply.github.com>
2023-04-26 16:25:35 -0700

    styling the footer

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,4 +1,9 @@
+import { useUser } from '@auth0/nextjs-auth0/client';
+import Image from 'next/image';
+import Link from 'next/link';
+
 export const AppLayout = ({ children }) => {
+  const { user } = useUser();
   return (
     <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
       <div className="flex flex-col text-white overflow-hidden">
@@ -10,7 +15,29 @@ export const AppLayout = ({ children }) => {
         <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
           list of posts
         </div>
-        <div className="bg-cyan-800">user information - logout button</div>
+        <div className="flex items-center gap-2 border-t border-t-black/50 h-20 px-2 bg-cyan-800">
+          {!!user ? (
+            <>
+              <div className="min-width-[50px]">
+                <Image
+                  className="rounded full"
+                  src={user.picture}
+                  alt={user.name}
+                  height={50}
+                  width={50}
+                />
+              </div>
+              <div className="flex-1">
+                <div className="font-bold">{user.email}</div>
+                <Link className="text-sm" href="/api/auth/logout">
+                  Logout
+                </Link>
+              </div>
+            </>
+          ) : (
+            <Link href="/api/auth/login">Login</Link>
+          )}
+        </div>
       </div>
       <div>{children}</div>
     </div>
