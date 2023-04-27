45b2ce810c0bfd67f786f4661f16592932a41a4a
casecam <109696610+casecam@users.noreply.github.com>
2023-04-27 11:44:38 -0700

    accepting user input

--- pages/api/generatePost.js
+++ pages/api/generatePost.js
@@ -5,16 +5,13 @@ export default async function handler(req, res) {
     apiKey: process.env.OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(config);
-
-  const topic = 'Tope 10 tips for dog owners';
-  const keywords =
-    'first-time dog owners, common dog health issues, best dog breeds';
+  const { topic, keywords } = req.body;
 
   const response = await openai.createCompletion({
     model: 'text-davinci-003',
     temperature: 0,
     max_tokens: 3600,
-    prompt: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following comma-separated keywords ${keywords}.
+    prompt: `Write a long and detailed SEO-friendly blog post about ${topic} that targets the following comma-separated keywords ${keywords}.
     The content should be formatted in SEO-friendly HTML.
     The response must also include appropriate HTML title and meta description content.
     The return format must be stringified JSON in the following format: 
--- pages/post/new.js
+++ pages/post/new.js
@@ -4,20 +4,48 @@ import { AppLayout } from '../../components/AppLayout';
 
 export default function NewPost(props) {
   const [postContent, setPostContent] = useState('');
-  const handleClick = async () => {
+  const [topic, setTopic] = useState('');
+  const [keywords, setKeywords] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     const res = await fetch(`/api/generatePost`, {
       method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({ topic, keywords }),
     }).then((res) => res.json());
-    console.log(res.post.postContent);
     setPostContent(res.post.postContent);
   };
 
   return (
     <div>
-      <h1>New Post Page</h1>
-      <button className="btn" onClick={handleClick}>
-        Generate
-      </button>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            <strong>Generate a blog post on the topic of:</strong>
+          </label>
+          <textarea
+            className="txt-area-input"
+            value={topic}
+            onChange={(e) => setTopic(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>
+            <strong>Targeting the following keywords:</strong>
+          </label>
+          <textarea
+            className="txt-area-input"
+            value={keywords}
+            onChange={(e) => setKeywords(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
       <div
         className="max-w-screen-sm p-10"
         dangerouslySetInnerHTML={{ __html: postContent }}
