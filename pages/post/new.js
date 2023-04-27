91f02931c1ba06ee7862e9af6b7ddf7dae375b52
casecam <109696610+casecam@users.noreply.github.com>
2023-04-27 11:22:28 -0700

    Hooking up openAI

--- pages/post/new.js
+++ pages/post/new.js
@@ -2,9 +2,19 @@ import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 import { AppLayout } from '../../components/AppLayout';
 
 export default function NewPost(props) {
+  const handleClick = async () => {
+    const res = await fetch(`/api/generatePost`, {
+      method: 'POST',
+    }).then(res => res.json());
+    console.log(res);
+  };
+
   return (
     <div>
       <h1>New Post Page</h1>
+      <button className="btn" onClick={handleClick}>
+        Generate
+      </button>
     </div>
   );
 }
