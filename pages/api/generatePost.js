2e9fcd29767d753f906ca1137fec5136bbc959b1
casecam <109696610+casecam@users.noreply.github.com>
2023-04-27 15:20:27 -0700

    open ai

--- pages/api/generatePost.js
+++ pages/api/generatePost.js
@@ -21,10 +21,87 @@ export default async function handler(req, res) {
       "metaDescription": meta description goes here
       }`,
   });
-  console.log('response', response);
-  res
-    .status(200)
-    .json({
-      post: JSON.parse(response.data.choices[0]?.text.split('\n').join('')),
-    });
+
+  const postContentResponse = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    temperature: 0,
+    messages: [
+      {
+        role: 'system',
+        content: 'You are a blog post generator',
+      },
+      {
+        role: 'user',
+        content: `Write a long and detailed SEO-friendly blog post about ${topic} that targets the following comma-separated keywords ${keywords}.
+        The content should be formatted in SEO-friendly HTML,
+        limited to the following HTML tags: p, h1, h2, h3, h4, h5, h6, strong, li, ol, ul, i.`,
+      },
+    ],
+  });
+
+  const postContent =
+    postContentResponse.data.choices[0]?.message?.content || '';
+
+  const titleResponse = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    temperature: 0,
+    messages: [
+      {
+        role: 'system',
+        content: 'You are a blog post generator',
+      },
+      {
+        role: 'user',
+        content: `Write a long and detailed SEO-friendly blog post about ${topic} that targets the following comma-separated keywords ${keywords}.
+        The content should be formatted in SEO-friendly HTML,
+        limited to the following HTML tags: p, h1, h2, h3, h4, h5, h6, strong, li, ol, ul, i.`,
+      },
+      {
+        role: 'assistant',
+        content: postContent,
+      },
+      {
+        role: 'user',
+        content: 'Generate appropriate title tag text for the above blog post.',
+      },
+    ],
+  });
+
+  const metaDescriptionResponse = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    temperature: 0,
+    messages: [
+      {
+        role: 'system',
+        content: 'You are a blog post generator',
+      },
+      {
+        role: 'user',
+        content: `Write a long and detailed SEO-friendly blog post about ${topic} that targets the following comma-separated keywords ${keywords}.
+        The content should be formatted in SEO-friendly HTML,
+        limited to the following HTML tags: p, h1, h2, h3, h4, h5, h6, strong, li, ol, ul, i.`,
+      },
+      {
+        role: 'assistant',
+        content: postContent,
+      },
+      {
+        role: 'user',
+        content:
+          'Generate SEO-friendly meta description content for the above blog post.',
+      },
+    ],
+  });
+
+  const title = titleResponse.data.choices[0]?.message?.content || '';
+  const metaDescription =
+    metaDescriptionResponse.data.choices[0]?.message?.content || '';
+
+  res.status(200).json({
+    post: {
+      postContent,
+      title,
+      metaDescription,
+    },
+  });
 }
